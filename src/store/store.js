66f8930f2eefc00761529ef3a5b4eb7b0c1e56ac
Meet Zaveri <meethpatel96@gmail.com>
2018-08-21 21:45:15 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux'
-import {mobile, resourcerequest_android} from './reducers/mobile';
+import {resourcerequest_android} from './reducers/mobile';
 import {employee_reducer} from './reducers/employee'
 import {projects} from './reducers/projects'
 
-let rootReducer = combineReducers({mobile, res_req_mob: resourcerequest_android, employee: employee_reducer, projects: projects});
+let rootReducer = combineReducers({res_req_mob: resourcerequest_android, employee: employee_reducer, projects: projects});
 
 export default rootReducer;
